b5c3a93b3bcb3598e9f9420edf6d929b396eab4f
dario617 <spam.dario@gmail.com>
2020-07-14 16:47:17 -0400

    Interface recovery for measurements

--- ansible/roles/measure/files/measure.sh
+++ ansible/roles/measure/files/measure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 replay_duration="10"
-netmap_delay="6"
+netmap_delay=0
 
 function do_replay() {
 	# Prepare tcpreplay flags
@@ -13,16 +13,18 @@ function do_replay() {
 	rflags="-K -q -T gtod -l 10000000 ${netmap} --duration=${duration} ${speed}"
 
 	# Stats before for each target
-	for host in targets:
+	for host in $targets; do
 		ssh $user@$host "cd ${wd}; ./gather_stats.sh before${host}.log"
+	done
 
 	# Replay the trace file
 	tcpreplay -i $1 ${rflags} $2
 	
 	# Stats after and compute
-	for host in targets:
+	for host in $targets; do
 		ssh $user@$host "cd ${wd}; ./gather_stats.sh after${host}.log"
 		ssh $user@$host "cd ${wd}; ./compute_rate.sh ${pps}"
+	done
 
 }
 
@@ -32,4 +34,6 @@ user=$3
 targets=$4 # Like "192.168.0.1 192.168.0.2 192.168.0.3"
 wd=$5
 
-do_replay pps pcap_file
+do_replay $pps $pcap_file
+
+../tools/measure.sh 170000 queries-ipv4rand.pcap dario "172.30.65.15 172.30.65.16 172.30.65.18" kvs-dns-bench
