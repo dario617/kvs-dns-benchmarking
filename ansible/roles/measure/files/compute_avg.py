3d41af595c8c9eb257376ea6473dca946a1294af
dario617 <spam.dario@gmail.com>
2020-07-22 00:25:16 -0400

    Add basic time query stats

---
+++ ansible/roles/measure/files/compute_avg.py
@@ -0,0 +1,35 @@
+import argparse
+import numpy as np
+
+def parseSec(name):
+  """
+  Results as in miliseconds
+  """
+  if name == "nsec":
+    return 1e-6
+  elif name == "msec":
+    return 1
+  elif name == "sec":
+    return 1e3
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Read response times as 0 msec and compute total stats")
+  parser.add_argument("f", type=str, help="File to read response times")
+  parser.add_argument('-o', "--output_file", help="Optional name for output", default="TimeStats.log")
+  args = parser.parse_args()
+
+  tmp = []
+  with open(args.f) as f:
+    print("Reading...")
+    for line in f:
+      vals = line.strip().split(" ")
+      tmp.append(float(vals[0])*parseSec(vals[1]))
+
+  total = np.array(tmp, dtype=np.float32)
+
+  with open(args.output_file, "w") as f:
+    f.write("Median {:f} msec\n".format(np.median(total)))
+    f.write("Average {:f} msec\n".format(np.mean(total)))
+    f.write("Std {:f} msec\n".format(np.std(total)))
+    f.write("Variance {:f} msec\n".format(np.var(total)))
+  print("Done")
---
+++ ansible/roles/measure/files/get_times.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source env_vars.sh
+
+function dig_zones {
+  # Wait for 20% of last zones
+	# Probabilistic, but digging 1M+ times is too slow and affects the results
+	zonelist="$1"
+	zonecount=$(cat ${zonelist}|wc -l)
+	portion=$(( ${zonecount} / 100 + 20 ))
+  
+  # Reset file
+  touch $3
+  echo "" > $3
+
+  for i in {$2..1}
+  do
+    while read zone;
+    do
+      ret=1
+      dig @$myIp -p $dnsPort +notcp $zone SOA | grep "Query time" | awk '{print $4, $5}' >> $3
+    done < <(tail -n ${portion} ${zonelist})
+  done
+}
+
+# As dig_zones file repeat out
+dig_zones $1 $2 $3
