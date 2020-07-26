d26e465227b2df13e2f2ddd617a9ae6d0b0841e3
dario617 <spam.dario@gmail.com>
2020-07-26 01:31:48 -0400

    Reset data utility, do mem snap before clean

---
+++ reset_data.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Author: Dario Andhael
+# Issues at dpalma@dcc.uchile.cl
+
+echo -e "
+           ######################
+           # Reset data utility #
+           ######################
+
+Clear parsed and random data from data folders
+Then continue with a pre_run playbook
+"
+cd ansible
+# Setup password for multiple playbooks
+read -p 'Do you want to store your password in a variable? (y/n) ' doPass
+if [ $doPass == "y" ]; then
+    read -sp 'Please type your password: ' daPasswd
+    echo ""
+else
+    echo "\__ you will be prompted for each test for your password"
+fi
+# Setup
+read -p 'Run? (y/n) ' doSetup
+if [ $doSetup == "y" ]; then
+    echo "Starting setup"
+    if [ -z $daPasswd ]; then
+        ansible-playbook -K playbooks/reset_dataset.yml
+        ansible-playbook -K playbooks/pre_run.yml
+    else
+        ansible-playbook playbooks/reset_dataset.yml --extra-vars "ansible_become_pass=${daPasswd}"
+        ansible-playbook playbooks/pre_run.yml --extra-vars "ansible_become_pass=${daPasswd}"
+    fi
+else
+    echo "Aborting... bye bye"
+fi
